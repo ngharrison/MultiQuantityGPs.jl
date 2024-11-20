f0577a9a9e6e5aca954b683913ba3ef083078d2b
Documenter.jl <documenter@juliadocs.github.io>
2024-11-20 20:33:21 +0000

    build based on 5574da2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MultiQuantityGPs","category":"page"},{"location":"#MultiQuantityGPs","page":"Home","title":"MultiQuantityGPs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MultiQuantityGPs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MultiQuantityGPs]","category":"page"}]
+}
